6ee491a0b7e77c8fb7d258f73dca1e97090b3314
Tisamu <nait.samuel@gmail.com>
2016-01-22 17:18:22 +0100

    Mis en ligne de ma mquette pour le challenge individuel

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+        
+    </head>
+    <body>
+        <header>
+            <a id="logo" href="#">
+                <img src="images/logo-icon-white.png">
+                <span>BONSAI</span>
+            </a>
+            
+            <nav>
+                <li id="login_button"><a class="button" href="#">LOGIN</a></li>
+                <li id="login_button"><a class="button" href="#">SIGN UP</a></li>
+            </nav>
+            
+            <div id="header_main">
+            <h1 class="header_title">Simple & Beautiful Payments for Freelances</h1>
+            <h3 class="header_title">Get paid on time, every time.</h3>
+            <a class="button" id="create_account" href="#">CREATE A FREE ACCOUNT</a>
+            </div>
+            
+        </header>
+        
+        <div id="summary">
+            <div class="summary_div">
+                <img class="summary_img" src="images/payments-calendar-icon.svg">
+                <h3>Quick</h3>
+                <p>to create, send, and pay invoices</p>
+            </div>
+            
+            <div class="summary_div">
+                <img class="summary_img" src="images/payments-mail-icon.svg">
+                <h3>Beautiful</h3>
+                <p>process for the freelancer and the client</p>
+            </div>
+            
+            <div class="summary_div">
+                <img class="summary_img" src="images/payments-methods-icon.svg">
+                <h3>Secure</h3>
+                <p>payments and quick disbursement</p>
+            </div>
+        </div>
+        
+        <div id="pricing_method">
+            <div id="pricing_title"><h3>SIMPLE PRICING:</h3><br>
+            <h3>PAY WHEN YOU GET PAID</h3>
+            </div>
+            <div id="price">
+                <h1 id="dollar">$1</h1>
+                <h2>PER PAID INVOICE</h2>
+                <h5>+ 2.9% & 30c transaction fee</h5>
+            </div>
+        </div>
+        
+        <p id="cc_support">Credit cards supported now. ACH coming soon!</p>
+        
+        
+        <!-- DEMO BEGIN -->
+        <div class="page_block"id="block_one">
+
+                <img id="page_img" src="images/welcome-invoices-graphic-1.png">
+        </div>
+        <div class="page_block" id="block_two">
+                <h3>Non quid quidvis possit amplissimos.</h3>
+                <p>
+                    Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+                </p>
+        </div>
+
+            <div class="page_block" id="block_three">
+                <h3>Non quid quidvis possit amplissimos.</h3>
+                <p>
+                    Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+                </p>
+            </div>
+            <div class="page_block" id="block_four">
+                <img id="page_img" src="images/welcome-invoices-graphic-2.png">
+            </div>
+
+
+            
+
+            <div class="page_block" id="block_five" style="margin-bottom: 5%;">>
+                <img id="page_img" src="images/welcome-invoices-graphic-3.png">
+            </div>
+            <div class="page_block" id="block_six" style="margin-bottom: 5%;">
+                <h3>Non quid quidvis possit amplissimos.</h3>
+                <p>
+                    Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+                </p>
+            </div>
+        <!-- DEMO END -->
+        <div  class="relance">
+            <h3>Ready to freelance like a pro?</h3>
+            <h3>Sign up here & get 5 <strong>FREE</strong> invoices.</h3>
+            <br><br>
+            <a id="create_account_bottom">CREATE AN ACCOUNT</a>
+        </div>
+        
+        <footer>
+                <h3>Contact</h3>
+                <p>10 Rue des carreaux, 1th Floor</p>
+                <p>Boulogne sur mer, CP 62200</p>
+            
+        </footer>
+        
+        <i id="bubble" class="fa fa-comments"></i>
+
+    </body>
+</html>
